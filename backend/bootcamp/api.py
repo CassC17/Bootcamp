bb5a981fab08e1bf9c5636b6b81f85c44b91a466
CassC17 <cassandra.cinna@supdevinci-edu.fr>
2024-11-06 11:49:13 +0100

    fix router + path error

--- backend/bootcamp/api.py
+++ backend/bootcamp/api.py
@@ -1,8 +1,8 @@
 from ninja import NinjaAPI
-from polls.api import router 
-from game.api import router
+from polls.api import router as polls_router
+from game.api import router as game_router
 
 api = NinjaAPI()
 
-api.add_router("/polls/", "polls.api.router")
-api.add_router("/game/", "game.api.router")
+api.add_router("/polls/", polls_router)
+api.add_router("/game/", game_router)
--- frontend/project/src/hooks/useCreateGame.jsx
+++ frontend/project/src/hooks/useCreateGame.jsx
@@ -1,15 +1,15 @@
 
 export default function useCreateGame(){
     const createGame = () => {
-        fetch(`http://localhost:8000/api/create_question/`, {
+        fetch(`http://localhost:8000/api/game/start_game/`, {
             method: "POST",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                question_text: "name",
-                choices: ["str"]
+                name: "name",
+                players: ["str"]
             }),
         
         })
